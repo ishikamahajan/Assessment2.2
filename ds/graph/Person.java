05256e1bbeb5741659ab15ca5925bfed1a4665f5
ishikamahajan <ishikamahajan7824@gmail.com>
2022-11-22 08:09:58 +0600

    person and socialgraph class

--- ds/graph/Person.java
+++ ds/graph/Person.java
@@ -1,11 +1,71 @@
 package ds.graph;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Person {
-	
+
+	private float socialHygine;
+	private int age;
+	private String name;
+	private ArrayList<Person> contacts;
+	private Business business;
+
 	public Person(String name, int age, float socialHygine) {
-		
+		this.name=name;
+		this.age=age;
+		this.socialHygine=socialHygine;
+		contacts=new ArrayList<>();
+		business=null;
 	}
-	
+
+	public Float getInfectiveness(){
+		return (float) ((socialHygine*(age/100.00))-(age/100.00));
+	}
+
+	public float getSocialHygine() {
+		return socialHygine;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ArrayList<Person> getContacts() {
+		return contacts;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Person)) return false;
+		Person person = (Person) o;
+		return Objects.equals(name, person.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public String toString() {
+		return "Person:" +
+				name + "," +
+				age + "  " +
+				"Business: " + (business==null?"[No Business Set]":business)+" "+
+				"Contacts: " + contacts.size();
+	}
+
+	public Business getBusiness() {
+		return business;
+	}
+
+	public void setBusiness(Business bus) {
+		this.business = bus;
+	}
 }
--- ds/graph/SocialGraph.java
+++ ds/graph/SocialGraph.java
@@ -1,33 +1,50 @@
 package ds.graph;
 
-import java.util.ArrayList;
+import ds.graph.exceptions.EdgeDoesNotExist;
+import ds.graph.exceptions.PersonAlreadyExists;
+import ds.graph.exceptions.PersonDoesNotExist;
+
+import java.util.*;
 
 public class SocialGraph {
+
+	private ArrayList<Person> vertices;
 	
 	public SocialGraph() {
-		
+		vertices=new ArrayList<>();
 	}
 	
 	/**
 	 * Add the given person to the graph. The person needs to be added to the list of vertices.
 	 * 
 	 * @param p
-	 * @throws PersonAlreadyExists If the person is already present in the graph, 
+	 * @throws PersonAlreadyExists If the person is already present in the graph,
 	 *  		this method should throw a PersonAlreadyPresent exception. 
 	 */
-	public void addVertex(Person p) {
-		
+	public void addVertex(Person p) throws PersonAlreadyExists {
+		if(vertices.contains(p)){
+			throw new PersonAlreadyExists("Person Already Exists");
+		}
+		vertices.add(p);
 	}
 	
 	
 	/**
 	 * Remove the given Person from the graph. Any edges to this person should also be removed. 
 	 * 
-	 * @throws PersonDoesNotExist If the given person is not found inside the graph. 
+	 * @throws PersonDoesNotExist If the given person is not found inside the graph.
 	 * @param p
 	 */
-	public void removeVertex(Person p) {
-		
+	public void removeVertex(Person p) throws PersonDoesNotExist {
+		if(!vertices.contains(p)){
+			throw new PersonDoesNotExist("Person Does Not Exist");
+		}
+
+		for (Person contact : p.getContacts()) {
+			contact.getContacts().remove(p);
+		}
+
+		vertices.remove(p);
 	}
 	
 	/**
@@ -39,10 +56,15 @@ public class SocialGraph {
 	 * @param a
 	 * @param b
 	 * 
-	 * @throws PersonDoesNotExist	If the person is not found within the graph. 
+	 * @throws PersonDoesNotExist If the person is not found within the graph.
 	 */
-	public void addEdge(Person a, Person b) {
-		
+	public void addEdge(Person a, Person b) throws PersonDoesNotExist {
+		if(vertices.contains(a) && vertices.contains(b)){
+			a.getContacts().add(b);
+			b.getContacts().add(a);
+		}else{
+			throw new PersonDoesNotExist("Person Does Not Exist");
+		}
 	}
 	
 	/**
@@ -53,8 +75,13 @@ public class SocialGraph {
 	 * @param a
 	 * @param b
 	 */
-	public void removeEdge(Person a, Person b) {
-		
+	public void removeEdge(Person a, Person b) throws EdgeDoesNotExist {
+		if(a.getContacts().contains(b) && b.getContacts().contains(a)){
+			a.getContacts().remove(b);
+			b.getContacts().remove(a);
+		}else{
+			throw new EdgeDoesNotExist("Edge Does Not Exist");
+		}
 	}
 	
 	/**
@@ -67,8 +94,43 @@ public class SocialGraph {
 	 * @param target
 	 * @return A list of nodes that must be traversed to get to target, from start. 
 	 */
-	public ArrayList<Person> searchBFS(Person start, Person target) {
-		return null;
+	public ArrayList<Person> searchBFS(Person start, Person target) throws PersonDoesNotExist {
+		if(vertices.contains(start) && vertices.contains(target)){
+			HashSet<Person> visited=new HashSet<>();
+			HashMap<Person,Person> parent=new HashMap<>();
+
+			Queue<Person> queue=new LinkedList<>();
+			queue.add(start);
+			visited.add(start);
+			parent.put(start,start);
+
+			while (!queue.isEmpty()){
+				Person u= queue.poll();
+
+				if(u.equals(target)){
+					ArrayList<Person> path=new ArrayList<>();
+					while (!u.equals(start)){
+						path.add(u);
+						u=parent.get(u);
+					}
+					Collections.reverse(path);
+					return path;
+				}
+
+				for (Person contact : u.getContacts()) {
+					if(!visited.contains(contact)){
+						visited.add(contact);
+						queue.add(contact);
+						parent.put(contact,u);
+					}
+				}
+			}
+
+			return null;
+
+		}else{
+			throw new PersonDoesNotExist("Person Does Not Exist");
+		}
 	}
 	
 	/**
@@ -81,8 +143,43 @@ public class SocialGraph {
 	 * @param target
 	 * @return A list of nodes that must be traversed to get to target, from start. 
 	 */
-	public ArrayList<Person> searchWeightedBFS(Person start, Person target) {
-		return null;
+	public ArrayList<Person> searchWeightedBFS(Person start, Person target) throws PersonDoesNotExist {
+		if(vertices.contains(start) && vertices.contains(target)){
+			HashSet<Person> visited=new HashSet<>();
+			HashMap<Person,Person> parent=new HashMap<>();
+
+			PriorityQueue<Person> queue=new PriorityQueue<>(Comparator.comparing(Person::getInfectiveness));
+			queue.add(start);
+			visited.add(start);
+			parent.put(start,start);
+
+			while (!queue.isEmpty()){
+				Person u= queue.poll();
+
+				if(u.equals(target)){
+					ArrayList<Person> path=new ArrayList<>();
+					while (!u.equals(start)){
+						path.add(u);
+						u=parent.get(u);
+					}
+					Collections.reverse(path);
+					return path;
+				}
+
+				for (Person contact : u.getContacts()) {
+					if(!visited.contains(contact)){
+						visited.add(contact);
+						queue.add(contact);
+						parent.put(contact,u);
+					}
+				}
+			}
+
+			return null;
+
+		}else{
+			throw new PersonDoesNotExist("Person Does Not Exist");
+		}
 	}
 	
 		
@@ -95,8 +192,46 @@ public class SocialGraph {
 	 * @param target
 	 * @return A list of nodes that must be traversed to get to target, from start. 
 	 */
-	public ArrayList<Person> searchDFS(Person start, Person target) {
-		return null;
+	public ArrayList<Person> searchDFS(Person start, Person target) throws PersonDoesNotExist {
+		if(vertices.contains(start) && vertices.contains(target)){
+			HashSet<Person> visited=new HashSet<>();
+			HashMap<Person,Person> parent=new HashMap<>();
+
+			Stack<Person> stack=new Stack<>();
+			stack.push(start);
+			visited.add(start);
+			parent.put(start,start);
+
+			while (!stack.isEmpty()){
+				Person u= stack.pop();
+
+				if(u.equals(target)){
+					ArrayList<Person> path=new ArrayList<>();
+					while (!u.equals(start)){
+						path.add(u);
+						u=parent.get(u);
+					}
+					Collections.reverse(path);
+					return path;
+				}
+
+
+				ArrayList<Person> contacts = u.getContacts();
+				for (int i = contacts.size()-1; i >= 0; i--) {
+					Person contact = contacts.get(i);
+					if (!visited.contains(contact)) {
+						visited.add(contact);
+						stack.push(contact);
+						parent.put(contact, u);
+					}
+				}
+			}
+
+			return null;
+
+		}else{
+			throw new PersonDoesNotExist("Person Does Not Exist");
+		}
 	}
 	
 	/**
@@ -109,8 +244,46 @@ public class SocialGraph {
 	 * @param target
 	 * @return A list of nodes that must be traversed to get to target, from start. 
 	 */
-	public ArrayList<Person> searchWeightedDFS(Person start, Person target) {
-		return null;
+	public ArrayList<Person> searchWeightedDFS(Person start, Person target) throws PersonDoesNotExist {
+		if(vertices.contains(start) && vertices.contains(target)){
+			HashSet<Person> visited=new HashSet<>();
+			HashMap<Person,Person> parent=new HashMap<>();
+
+			Stack<Person> stack=new Stack<>();
+			stack.push(start);
+			visited.add(start);
+			parent.put(start,start);
+
+			while (!stack.isEmpty()){
+				Person u= stack.pop();
+
+				if(u.equals(target)){
+					ArrayList<Person> path=new ArrayList<>();
+					while (!u.equals(start)){
+						path.add(u);
+						u=parent.get(u);
+					}
+
+					Collections.reverse(path);
+					return path;
+				}
+
+				u.getContacts().sort(Comparator.comparing(Person::getInfectiveness));
+
+				for (Person contact : u.getContacts()) {
+					if(!visited.contains(contact)){
+						visited.add(contact);
+						stack.push(contact);
+						parent.put(contact,u);
+					}
+				}
+			}
+
+			return null;
+
+		}else{
+			throw new PersonDoesNotExist("Person Does Not Exist");
+		}
 	}
 	
 	/**
@@ -122,7 +295,19 @@ public class SocialGraph {
 	 * @param start
 	 * @return
 	 */
-	public int countContacts(Person start) {
-		return 0;
+	public int countContacts(Person start) throws PersonDoesNotExist {
+		if(!vertices.contains(start)){
+			throw new PersonDoesNotExist("Person Does Not Exist");
+		}
+
+		HashSet<Person> people=new HashSet<>();
+
+		for (Person contact : start.getContacts()) {
+			for (Person contactContact : contact.getContacts()) {
+				if(!contactContact.equals(start))people.add(contactContact);
+			}
+		}
+
+		return people.size();
 	}
 }
